c5435cb1e5b3d03c595a18c716fbaae096290e79
Nicolas M <mine.nicolas@gmail.com>
2023-11-11 22:41:55 +0100

    initial commit - setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import AboutMe from './components/about_me/AboutMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <div className="Section">
+        <AboutMe/>
+      </div>
+      <div className="Section">
+        <AboutMe/>
+      </div>
+      <div className="Section">
+        <AboutMe/>
+      </div>
+      <div className="Section">
+        <AboutMe/>
+      </div>
     </div>
   );
 }
---
+++ src/components/about_me/AboutMe.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './AboutMe.scss';
+
+function AboutMe() {
+  return (
+    <a>TODO</a>
+  );
+}
+
+export default AboutMe;
